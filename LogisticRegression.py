d4464d0894539d07a2615255867e09ec7b8c9f1d
Alex Stedman <stedman24@gmail.com>
2019-09-27 18:52:28 -0400

    K fold working

--- LogisticRegression.py
+++ LogisticRegression.py
@@ -2,6 +2,7 @@ import Preprocess
 import CrossValidate
 import matplotlib
 import numpy as np
+import random
 
 
 class LogisticRegression:
@@ -12,15 +13,15 @@ class LogisticRegression:
                           'density', 'pH', 'sulphates', 'alcohol']
         self.wine_weights = []
         for i in range(0, len(self.wine_data[0])):
-            self.wine_weights.append(0)
-        self.wine_weights = np.array(self.wine_weights)
+            self.wine_weights.append(0.0)
+        self.wine_weights = np.array(self.wine_weights, dtype=float)
 
         self.cancer_data = Preprocess.preprocessTumour()
         self.cancer_vars = ['']
         self.cancer_weights = []
         for j in range(0, len(self.cancer_data[0])):
-            self.cancer_weights.append(0)
-        self.cancer_weights = np.array(self.cancer_weights)
+            self.cancer_weights.append(0.0)
+        self.cancer_weights = np.array(self.cancer_weights, dtype=float)
         # print(list(enumerate(self.cancer_weights)))
         # print(self.cancer_data[0])
         # self.predict(self.cancer_weights, self.cancer_data[0][:-1], self.cancer_data[0][-1])
@@ -38,8 +39,8 @@ class LogisticRegression:
 
 
     @staticmethod
-    def learn_rate(iterations):
-        return 1 / (iterations * iterations)
+    def learn_rate(iterations, data_length):
+        return 1 / (data_length*data_length*1000000)
 
     # @staticmethod
     # def train(data, weights, iterations):
@@ -55,7 +56,7 @@ class LogisticRegression:
         # add w0 above, eliminate w0 value from array below, so loop does not add last value to the sum
         for count, weight in np.ndenumerate(w[1:]):
             # print(count, weight, x[count])
-            a_out = a_out + w[count] + weight
+            a_out = a_out + x[count] * weight
         return a_out
 
     @staticmethod
@@ -67,20 +68,24 @@ class LogisticRegression:
         return 0
 
     @staticmethod
-    def fit(w, x_in, indicator, learning_rate):
+    def fit(w, x_in, learning_rate):
         to_add = 0
+        w.astype(np.float)
         w_k1 = np.copy(w)
-        for count in range(len(x_in)):
-            for wt_count in range(len(w)):
-                w_k1[count] = w[count] \
-                              + learning_rate(count) * x_in[count][wt_count] * (
-                                      indicator - LogisticRegression.sigmoid(LogisticRegression.linear_fn_eval(w, x_in))
-                              )  # Update Rule
+        for count in range(len(w)):
+            for wt_count in range(len(x_in)):
+                x_vals = x_in[wt_count][:-1]
+                # indicator = LogisticRegression.predict(w, x_in[wt_count])
+                indicator = x_in[wt_count][-1]
+                inner_part = indicator - LogisticRegression.sigmoid(LogisticRegression.linear_fn_eval(w, x_in[wt_count]))
+                to_add = to_add + learning_rate(count, len(x_in)) * x_in[wt_count][count] * inner_part  # Update Rule
+            w_k1[count] = w[count] + to_add
+            to_add = 0
         return w_k1
 
-    @staticmethod
-    def loss(h, y):
-        return (-y * np.log(h) - (1 - y) * np.log(1 - h)).mean()
+    # @staticmethod
+    # def loss(h, y):
+    #     return (-y * np.log(h) - (1 - y) * np.log(1 - h)).mean()
 
 
 def evaluate_acc(y_actual, y_predicted):
@@ -100,14 +105,62 @@ def create_y_lists(data, weights):
         y_actual.append(x[-1])
     return y_actual, y_predicted
 
+def get_folds(lr, data_in, k):
+    # Setting up different folds
+    data = data_in
+    split_size = round(len(data) / k)
+    learn_set = [[]] * k
+    test_set = [[]] * k
+    for count in range(k):
+        learn_set[count] = []
+        test_set[count] = []
+    for i_k in range(k):
+        for j in range(len(data)):
+            if (j % k) == i_k:
+                test_set[i_k].append(data[j].tolist())
+            else:
+                learn_set[i_k].append(data[j].tolist())
+    return learn_set, test_set
+
+def k_fold_cancer(lr):
+    data_set = lr.cancer_data
+    k = 5
+    learn_l, test_l = get_folds(lr, data_set, k)
+    for i in range(k):
+        # each iteration uses a different set of data
+        learn = np.asarray(learn_l[i])
+        test = np.asarray(test_l[i])
+        lr.cancer_weights = LogisticRegression.fit(lr.cancer_weights, learn, LogisticRegression.learn_rate)
+        y_act, y_pred = create_y_lists(test, lr.cancer_weights)
+        print('CANCER: {}'.format(evaluate_acc(y_act, y_pred)))
+
+def k_fold_wine(lr):
+    data_set = lr.wine_data
+    k = 5
+    learn_l, test_l = get_folds(lr, data_set, k)
+    for i in range(k):
+        # each iteration uses a different set of data
+        learn = np.asarray(learn_l[i])
+        test = np.asarray(test_l[i])
+        lr.wine_weights = LogisticRegression.fit(lr.wine_weights, learn, LogisticRegression.learn_rate)
+        y_act, y_pred = create_y_lists(test, lr.wine_weights)
+        print('WINE: {}'.format(evaluate_acc(y_act, y_pred)))
+
+
 
 def main():
     lr = LogisticRegression()
-    y_act, y_pred = create_y_lists(lr.cancer_data, lr.cancer_weights)
-    print(evaluate_acc(y_act, y_pred))
-    lr.cancer_weights = LogisticRegression.fit(lr.cancer_weights, lr.cancer_data, LogisticRegression.learn_rate, )
-    y_act, y_pred = create_y_lists(lr.cancer_data, lr.cancer_weights)
-    print(evaluate_acc(y_act, y_pred))
+    k_fold_cancer(lr)
+    k_fold_wine(lr)
+    # y_act, y_pred = create_y_lists(lr.wine_data, lr.wine_weights)
+    # print(evaluate_acc(y_act, y_pred))
+    # num_iterations = 1000
+    # for x in range(num_iterations):
+    #     lr.wine_weights = LogisticRegression.fit(lr.wine_weights, lr.wine_data, LogisticRegression.learn_rate)
+    #     y_act, y_pred = create_y_lists(lr.wine_data, lr.wine_weights)
+    #     print(lr.wine_weights)
+    #     print(evaluate_acc(y_act, y_pred))
+
 
 
 if __name__ == "__main__":
