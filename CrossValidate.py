156073824385e3502b3a831b43a39adddb73d70c
KartoffelKopf <ethan.anderson995@gmail.com>
2019-09-28 19:36:08 -0400

    LDA first try

--- CrossValidate.py
+++ CrossValidate.py
@@ -33,7 +33,7 @@ def kFoldCrossValidation(datasets, fit, predict, evaluate_acc):
         predictedValues = predict(validationSet, weights)
 
         # check how accurate the model is
-        errors.append(evaluate_acc(validationSet[:len(validationSet)-1], predictedValues))
+        errors.append(evaluate_acc(validationSet, predictedValues))
     
     #return the average of the errors
     return np.mean(errors)
--- LDA.py
+++ LDA.py
@@ -4,61 +4,54 @@ import numpy as np
 
 def fit(trainingSet, alpha=0):
 
-    n1 = np.sum(trainingSet[:,-1])
-    n0 = trainingSet.shape[1] - n1
-    prob1 = n1 / trainingSet.shape[1]
+    class1 = []
+    class0 = []
+    for row in trainingSet:
+        if row[-1] == 1:
+            class1.append(row[:-1])
+        else:
+            class0.append(row[:-1])
+    n1 = len(class1)
+    n0 = len(class0)
+    prob1 = n1 / (n1 + n0)
     prob0 = 1 - prob1
+    fmean0 = np.mean(class0, axis=0)
+    fmean1 = np.mean(class1, axis=0)
+    cov0 = np.cov(class0, rowvar=False, bias=False)
+    cov1 = np.cov(class1, rowvar=False, bias=False)
+    covar = (cov0*n0 + cov1*n1)/(n0+n1+2)
 
-    sum0 = np.zeros(trainingSet.shape[0] - 1)
-    sum1 = np.zeros(trainingSet.shape[0] - 1)
-    for row in trainingSet:
-        if row[-1]:
-            sum1 = np.add(sum1,row[:-1])
-        else:
-            sum0 = np.add(sum0,row[:-1])
-    fmean0 = sum0 / n0
-    fmean1 = sum1 / n1
-
-    covar = np.zeros((fmean1.shape[0], fmean1.shape[0]))
-    for row in trainingSet:
-        if row[-1]:
-            covar += (row[:-1] - fmean1) * np.transpose(row[:-1] - fmean1)
-        else:
-            covar += (row[:-1] - fmean0) * np.transpose(row[:-1] - fmean0)
-    covar = covar / (n1 + n0 - 2)
 
     return [prob0, prob1, fmean0, fmean1, covar]
 
 def predict(validationSet, params):
 
+    odds = []
     predictions = []
+    bias = np.log(params[1]/params[0]) + 0.5*params[2].T @ np.linalg.inv(params[4]) @ params[2] - 0.5*params[3].T @ np.linalg.inv(params[4]) @ params[3]
     for row in validationSet:
-        predictions.append(
-            np.log(params[0]/params[1])
-            + 0.5*np.multiply(params[2],np.multiply(np.transpose(params[2]),params[4]))
-            - 0.5*np.multiply(params[3],np.multiply(np.transpose(params[3]),params[4]))
-            + np.multiply(np.multiply(np.transpose(validationSet[:-1]),params[4]),(params[3] - params[2]))
-        )
+        odds.append(bias + row[:-1]@np.linalg.inv(params[4])@(params[3]-params[2]))
 
-    for odds in predictions:
-        if odds >= 0:
-            odds = 1
+    for number in odds:
+        if number >= 0:
+            predictions.append(1)
         else:
-            odds = 0
-
-    return  predictions
+            predictions.append(0)
+    return predictions
 
 def evaluate_acc(actualValues, predictedValues):
-    return ((actualValues==1 and predictedValues==1) or (actualValues==0 and predictedValues==0)).sum()/actualValues.len()
+    return np.sum(np.logical_and(actualValues[:,-1], predictedValues))/len(predictedValues)
+
 
 wineDataset = Preprocess.preprocessWine()
 wineDatasets = CrossValidate.split(wineDataset, 5)
 
-# GradientDescent class should have function "fit" with training set and some gradient descent parameter alpha as inputs
-# GradientDescent class also has a predict and evaluate_acc function (see assignment pdf)
 averageErrorWine = CrossValidate.kFoldCrossValidation(wineDatasets, fit, predict, evaluate_acc)
 
 tumourDataset = Preprocess.preprocessTumour()
 tumourDatasets = CrossValidate.split(tumourDataset, 5)
 
 averageErrorTumour = CrossValidate.kFoldCrossValidation(tumourDatasets, fit, predict, evaluate_acc)
+
+print("Wine accuracy: " + str(averageErrorWine))
+print("Tumor accuracy: " + str(averageErrorTumour))
